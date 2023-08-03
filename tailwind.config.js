b37c08efc7ddf6994508d99a01ba5a99ca18fd3b
kohicha <107725512+kohicha@users.noreply.github.com>
2023-08-04 01:50:04 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily:{
+      BigShoulderDisplay: 'BigShouldersDisplay',
+      LexandDeca: "LexandDeca"
+    }
+    ,
+    extend: {},
+  },
+  plugins: [],
+}
+
